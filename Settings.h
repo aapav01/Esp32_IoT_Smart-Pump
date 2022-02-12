7da2e9b8c02c0e8fb22043f3648e5f491a253c27
Apavayan Sinha <info@apavayan.com>
2022-02-12 17:31:10 +0530

    Smart-pump: Settings: Using Anode RBG Led with 8Bit colours
    Signed-off-by: Apavayan Sinha <info@apavayan.com>

--- Settings.h
+++ Settings.h
@@ -5,57 +5,57 @@
 
 #if defined(USE_WROVER_BOARD)
 
-  #define BOARD_BUTTON_PIN            15
-  #define BOARD_BUTTON_ACTIVE_LOW     true
+#define BOARD_BUTTON_PIN            15
+#define BOARD_BUTTON_ACTIVE_LOW     true
 
-  #define BOARD_LED_PIN_R             0
-  #define BOARD_LED_PIN_G             2
-  #define BOARD_LED_PIN_B             4
-  #define BOARD_LED_INVERSE           false
-  #define BOARD_LED_BRIGHTNESS        128
+#define BOARD_LED_PIN_R             0
+#define BOARD_LED_PIN_G             2
+#define BOARD_LED_PIN_B             4
+#define BOARD_LED_INVERSE           false
+#define BOARD_LED_BRIGHTNESS        128
 
 #elif defined(USE_TTGO_T7)
 
-  // This board does not have a built-in button
-  // Connect a button to gpio0 <> GND
-  #define BOARD_BUTTON_PIN            0
-  #define BOARD_BUTTON_ACTIVE_LOW     true
+// This board does not have a built-in button
+// Connect a button to gpio0 <> GND
+#define BOARD_BUTTON_PIN            0
+#define BOARD_BUTTON_ACTIVE_LOW     true
 
-  #define BOARD_LED_PIN               19
-  #define BOARD_LED_INVERSE           false
-  #define BOARD_LED_BRIGHTNESS        64
+#define BOARD_LED_PIN               19
+#define BOARD_LED_INVERSE           false
+#define BOARD_LED_BRIGHTNESS        64
 
 #else
 
-  #warning "Custom board configuration is used"
+#warning "Custom board configuration is used"
 
-  #define BOARD_BUTTON_PIN            0                     // Pin where user button is attached
-  #define BOARD_BUTTON_ACTIVE_LOW     true                  // true if button is "active-low"
+#define BOARD_BUTTON_PIN            0                     // Pin where user button is attached
+#define BOARD_BUTTON_ACTIVE_LOW     true                  // true if button is "active-low"
 
-  #define BOARD_LED_PIN               4                     // Set LED pin - if you have a single-color LED attached
-  //#define BOARD_LED_PIN_R           15                    // Set R,G,B pins - if your LED is PWM RGB
-  //#define BOARD_LED_PIN_G           12
-  //#define BOARD_LED_PIN_B           13
-  //#define BOARD_LED_PIN_WS2812      4                     // Set if your LED is WS2812 RGB
-  #define BOARD_LED_INVERSE           false                 // true if LED is common anode, false if common cathode
-  #define BOARD_LED_BRIGHTNESS        64                    // 0..255 brightness control
+//#define BOARD_LED_PIN               4                     // Set LED pin - if you have a single-color LED attached
+#define BOARD_LED_PIN_R               12                    // Set R,G,B pins - if your LED is PWM RGB
+#define BOARD_LED_PIN_G               15
+#define BOARD_LED_PIN_B               2
+//#define BOARD_LED_PIN_WS2812      4                     // Set if your LED is WS2812 RGB
+#define BOARD_LED_INVERSE           true                 // true if LED is common anode, false if common cathode
+#define BOARD_LED_BRIGHTNESS        64                    // 0..255 brightness control
 
 #endif
 
 
 /*
- * Advanced options
- */
+   Advanced options
+*/
 
 #define BUTTON_HOLD_TIME_INDICATION   3000
 #define BUTTON_HOLD_TIME_ACTION       10000
 
-#define BOARD_PWM_MAX                 1023
+#define BOARD_PWM_MAX                 255
 
 #define BOARD_LEDC_CHANNEL_1     1
 #define BOARD_LEDC_CHANNEL_2     2
 #define BOARD_LEDC_CHANNEL_3     3
-#define BOARD_LEDC_TIMER_BITS    10
+#define BOARD_LEDC_TIMER_BITS    8
 #define BOARD_LEDC_BASE_FREQ     12000
 
 #define CONFIG_AP_URL                 "blynk.setup"
@@ -77,7 +77,7 @@
 #define BLYNK_NO_DEFAULT_BANNER
 
 #if defined(APP_DEBUG)
-  #define DEBUG_PRINT(...) BLYNK_LOG1(__VA_ARGS__)
+#define DEBUG_PRINT(...) BLYNK_LOG1(__VA_ARGS__)
 #else
-  #define DEBUG_PRINT(...)
+#define DEBUG_PRINT(...)
 #endif
